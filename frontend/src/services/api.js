d5131ae439c1beecaa77568266c3e3244eba7a25
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2026-05-13 09:19:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000'
-})
+  baseURL: "https://ai-form-type-detector.onrender.com"
+});
 
-export default API
+export default API;
